a8e0956e21e4cf5fce9559813155cc4128fc6d33
dnitecki <dominick.nitecki@outlook.com>
2024-09-27 22:49:12 -0400

    add form styles

--- src/react/components/App.tsx
+++ src/react/components/App.tsx
@@ -1,10 +1,16 @@
 import "./App.scss";
+import RuleForm from "./RuleForm";
+import RepeatIcon from "@mui/icons-material/Repeat";
 
 export default function App() {
   return (
     <div className="app-container">
       <section className="header">
         <h1>Request Redirect</h1>
+        <RepeatIcon fontSize="large" />
+      </section>
+      <section className="main">
+        <RuleForm />
       </section>
     </div>
   );
---
+++ src/react/components/RuleForm.tsx
@@ -0,0 +1,32 @@
+import "./RuleForm.scss";
+import AddIcon from "@mui/icons-material/Add";
+
+const RuleForm = () => {
+  return (
+    <form id="ruleForm" className="rule-form-container">
+      <label htmlFor="fromUrl" className="form-label">
+        From URL:
+      </label>
+      <input
+        type="text"
+        id="fromUrl"
+        className="form-input"
+        placeholder="https://example.com"
+      />
+      <label htmlFor="toUrl" className="form-label">
+        To URL:
+      </label>
+      <input
+        type="text"
+        id="toUrl"
+        className="form-input"
+        placeholder="https://new-url.com"
+      />
+      <button type="submit" className="form-submit">
+        Add Rule <AddIcon fontSize="medium" />
+      </button>
+    </form>
+  );
+};
+
+export default RuleForm;
