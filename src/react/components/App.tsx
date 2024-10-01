990d19cdd6de11b77efd0df3006706e4e92cc1e5
dnitecki <dominick.nitecki@outlook.com>
2024-10-01 00:39:14 -0400

    update styles and add version to header

--- src/react/components/App.tsx
+++ src/react/components/App.tsx
@@ -2,17 +2,23 @@ import "./App.scss";
 import RuleForm from "./RuleForm";
 import RepeatIcon from "@mui/icons-material/Repeat";
 import RuleList from "./RuleList";
+import { version } from "../../../package.json";
 
 export default function App() {
   return (
     <div className="app-container">
       <section className="header">
-        <h1>ReRoute</h1>
-        <RepeatIcon fontSize="large" />
+        <div className="title">
+          <h1>ReRoute</h1>
+          <RepeatIcon fontSize="large" />
+        </div>
+        <div className="version">
+          <p>v{version}</p>
+        </div>
       </section>
       <section className="main">
         <RuleForm />
-        <div className="header">
+        <div className="title">
           <h1>Rules</h1>
         </div>
         <RuleList />
